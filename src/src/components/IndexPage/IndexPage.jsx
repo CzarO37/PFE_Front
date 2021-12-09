994c0363cc27d9eb364e12034a3aeb5876b7ea1e
Mateusz Rydzewski <mateusz.rydzewski@student.vinci.be>
2021-12-09 23:57:51 +0100

    Add - Index Page

---
+++ src/src/components/IndexPage/IndexPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Carrousel from '../Carrousel/Carrousel'
+import Navbar from '../Navbar/Navbar'
+import Presentation from '../Presentation/Presentation'
+
+const IndexPage = () => {
+    return (
+        <>
+        
+            <Navbar/>
+            <Carrousel/>
+            <Presentation/>
+        
+        </>
+    )
+}
+
+export default IndexPage
