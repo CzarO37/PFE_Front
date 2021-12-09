58b43a4e7e7ea8e1d636cc7f87ddd2d79a349f54
Mateusz <mateusz.rydzewski@student.vinci.be>
2021-12-09 09:43:40 +0100

    add - button

--- src/src/components/Button/Button.jsx
+++ src/src/components/Button/Button.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import './Button.css'
 
-const Button = ({name}) => (<button>{name}</button>)
+const Button = ({name}) => (<button class="button-green" id="login-btn">{name}</button>)
 
 export default Button
