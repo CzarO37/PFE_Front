1c7abc2c4a44db07ab94f488eb3915519fbebd6e
Mateusz Rydzewski <mateusz.rydzewski@student.vinci.be>
2021-12-11 12:20:43 +0100

    Edit - reorganized folder structure

---
+++ src/src/Components/IndexPage/Carrousel/Carrousel.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import tondeuse from "../../../images/products/tondeuse.jpg"
+import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+
+const Carrousel = () =>{
+    return (
+        <Grid container justifyContent="space-around" alignItems="center">
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    component="img"
+                    image={tondeuse}
+                />
+                <CardContent style={{backgroundColor : '#C6DFBA'}}>
+                    <Typography>Tondeuse Bosh 400x</Typography>
+                    <Typography>250€</Typography>
+                </CardContent>
+            </Card>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    component="img"
+                    width="20"
+                    height="auto"
+                    image={tondeuse}
+                />
+                <CardContent style={{backgroundColor : '#C6DFBA'}}>
+                    <Typography>Tondeuse Bosh 400x</Typography>
+                    <Typography>250€</Typography>
+                </CardContent>
+            </Card>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    component="img"
+                    width="20"
+                    height="auto"
+                    image={tondeuse}
+                />
+                <CardContent style={{backgroundColor : '#C6DFBA'}}>
+                    <Typography>Tondeuse Bosh 400x</Typography>
+                    <Typography>250€</Typography>
+                </CardContent>
+            </Card>
+        </Grid>
+    )
+}
+
+export default Carrousel
