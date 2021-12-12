08f0380d76006cad5c5077aa5f57ee30ddeddee6
Mateusz Rydzewski <mateusz.rydzewski@student.vinci.be>
2021-12-12 14:42:47 +0100

    Add - annimations

--- src/src/Components/IndexPage/Presentation/Presentation.jsx
+++ src/src/Components/IndexPage/Presentation/Presentation.jsx
@@ -1,5 +1,5 @@
 import { Button, Grid, Typography } from '@mui/material'
-import React from 'react'
+import React,{useEffect} from 'react'
 import vinci_logo from '../../../images/icon_logos/vinci_logo.png'
 import liens from '../../../images/icon_logos/creer_lien.png'
 import economiser from '../../../images/icon_logos/economiser.png'
@@ -7,11 +7,17 @@ import environnement from '../../../images/icon_logos/Respecter_environnement.pn
 import recuperation from '../../../images/icon_logos/privilegier_recuperation.png'
 import second_hand from '../../../images/icon_logos/seconde_main.png'
 import Image from 'mui-image'
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Presentation = () => {
+    useEffect(() => {
+        Aos.init({duration: 2000});
+    }, [])
+    
     return (
         <Grid container>
-            <Grid container justifyContent="center" sx={{padding:'100px'}}>
+            <Grid data-aos="fade-up" container justifyContent="center" sx={{padding:'100px'}}>
                 <Grid item xs={5}>
                 <Image src={vinci_logo}/>
                 </Grid>
@@ -29,41 +35,41 @@ const Presentation = () => {
                 </Grid>
             </Grid>
         <Grid container spacing={8} >
-            <Grid item xs={12} align-content="flex-end">
+            <Grid data-aos="fade-up" item xs={12} align-content="flex-end">
                 <Typography sx={{textAlign: 'center', fontSize:'30px', color:'#8CB17E', fontWeight:'bold'}}>Vinci2ndHand c'est donc :</Typography>
             </Grid>
-            <Grid item xs={8} justifyContent="flex-start">
+            <Grid data-aos="fade-right" item xs={8} justifyContent="flex-start">
                 <Image src={second_hand} sx={{maxWidth : "450px"}}/>
                 
             </Grid>
-            <Grid xs={4} sx={{display: "flex",flexDirection: "column",justifyContent: "center"}}>
+            <Grid  data-aos="fade-rigth" xs={4} sx={{display: "flex",flexDirection: "column",justifyContent: "center"}}>
                 <Typography sx={{fontSize:'40px', color:'#8CB17E', fontWeight:'bold'}}>Acheter en seconde main</Typography>
             </Grid>
-            <Grid xs={4} sx={{display: "flex",flexDirection: "column",justifyContent: "center"}}>
+            <Grid data-aos="fade-left" xs={4} sx={{display: "flex",flexDirection: "column",justifyContent: "center"}}>
                 <Typography sx={{textAlign: 'right', fontSize:'40px', color:'#8CB17E', fontWeight:'bold'}}>Créer des liens</Typography>
             </Grid>
-            <Grid item xs={8}>
+            <Grid data-aos="fade-left" item xs={8}>
                 <Image src={liens} sx={{maxWidth : "450px"}}/>
             </Grid>
-            <Grid item xs={8}>
+            <Grid data-aos="fade-right" item xs={8}>
                 <Image src={recuperation} sx={{maxWidth : "450px"}}/>
             </Grid>
-            <Grid xs={4} sx={{display: "flex",flexDirection: "column",justifyContent: "center"}}>
+            <Grid data-aos="fade-right" xs={4} sx={{display: "flex",flexDirection: "column",justifyContent: "center"}}>
                 <Typography sx={{fontSize:'40px', color:'#8CB17E', fontWeight:'bold'}}>Privilégier la récupération</Typography>
             </Grid>
-            <Grid xs={5} sx={{display: "flex",flexDirection: "column",justifyContent: "center"}}>
+            <Grid data-aos="fade-left" xs={5} sx={{display: "flex",flexDirection: "column",justifyContent: "center"}}>
                 <Typography sx={{textAlign: 'right', fontSize:'40px', color:'#8CB17E', fontWeight:'bold'}}>Respecter notre environnement</Typography>
             </Grid>
-            <Grid item xs={7}>
+            <Grid data-aos="fade-left" item xs={7}>
                 <Image src={environnement} sx={{maxWidth : "450px"}}/>
             </Grid>
-            <Grid item xs={8} >
+            <Grid data-aos="fade-right" item xs={8} >
                 <Image src={economiser} sx={{maxWidth : "450px"}}/>
             </Grid>
-            <Grid xs={4} sx={{display: "flex",flexDirection: "column",justifyContent: "center"}}>
+            <Grid data-aos="fade-right" xs={4} sx={{display: "flex",flexDirection: "column",justifyContent: "center"}}>
                 <Typography sx={{ fontSize:'40px', color:'#8CB17E', fontWeight:'bold'}}>Economiser</Typography>
             </Grid>
-            <Grid xs={12}>
+            <Grid data-aos="fade-up" xs={12}>
                 <Typography sx={{textAlign:'center', fontSize:'30px', color:'#8CB17E', fontWeight:'bold', padding:'150px', fontStyle:'italic'}}>
                     « Nous avons tous des trésors enfouis au fond de notre grenier ou de nos placards. 
                     Trésors qui profiteraient à d’autres.Valorisons-les en leur donnant une seconde vie. »
