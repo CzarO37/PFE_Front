1ab268f39a9e7a84c0de79d156d712e829f1f1e1
Thibaut Gérard <thibaut.t.gerard@student.vinci.be>
2021-12-13 17:51:40 +0100

    feat - add route to home page in navabr logo

--- src/src/components/Navbar/Navbar.jsx
+++ src/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
-import {Button, Grid} from "@mui/material";
+import {Avatar, Button, Grid} from "@mui/material";
 import React from "react";
-import logo_vinci from "../../images/icon_logos/logo_vinci2hand.png"
+import {Link} from 'react-router-dom'
+import logo from "../../images/vinci2ndhand.png";
+import logo_vinci from "../../images/icon_logos/logo_vinci2hand.png";
 
 const Navbar = () =>{
     return(
         <Grid container justifyContent="space-between" alignItems="center" marginBottom="50px">
             <Grid item>
-                <img src={logo_vinci} width="300px" height="auto"/>
+                <Link to="/">
+                    <img src={logo} width="300px" height="auto"/>
+                </Link>
             </Grid>
             <Grid item>        
                 <Button href={"/signup"} variant="contained" style={{background: 'linear-gradient(129deg, rgba(152,200,100,1) 0%, rgba(5,138,174,1) 84%, rgba(5,90,120,1) 100%)'}}>Se connecter</Button>
