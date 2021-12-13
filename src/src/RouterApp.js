d581f59abc5629e0ea902421ab0385b52ab2ceff
Thibaut Gérard <thibaut.t.gerard@student.vinci.be>
2021-12-13 14:53:33 +0100

    refactor - remove component App

--- src/src/RouterApp.js
+++ src/src/RouterApp.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Switch, Route, useRouteMatch, Link } from 'react-router-dom'
 
-import App from './Components/App/App'
 import LoginPage from "./Components/LoginPage/LoginPage";
 import SignUpPage from "./Components/SignUpPage/SignUpPage";
 
