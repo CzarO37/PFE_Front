306e5f966714fe4d54abd4f22ac7ea7a7b23aece
Thibaut Gérard <thibaut.t.gerard@student.vinci.be>
2021-12-13 14:36:26 +0100

    refactor - reformat responsive loginPage

--- src/src/Components/LoginPage/LoginPage.jsx
+++ src/src/Components/LoginPage/LoginPage.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
 import usersService from '../../services/users.js'
-import {Grid,Paper,Avatar,Container,Typography,TextField,Button} from '@mui/material'
+import {Grid, Paper, Avatar, Container, Typography, TextField, Button} from '@mui/material'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import 'fontsource-roboto';
 import logo from '../../images/vinci2ndhand.png'
+import './login.css'
 
 const LoginPage = () => {
 
     const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('') 
+    const [password, setPassword] = useState('')
 
     const handleEmailChange = (e) => {
         e.preventDefault()
@@ -30,33 +31,25 @@ const LoginPage = () => {
             password: password
         }
         usersService.loginUser(user)
-        .then(response => console.log(response))
+            .then(response => console.log(response))
     }
 
-    const backgroundStyle= {
-        position: 'fixed',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        top: 0,
-        'borderRadius':'0px',
-        'background': 'linear-gradient(129deg, rgba(152,200,100,1) 0%, rgba(5,138,174,1) 68%, rgba(5,90,120,1) 100%)',}
-    
-    const loginFormStyle = {
-        padding:20,
-        height:'50vh',
-        'borderTopRightRadius':'0 0',
-        'borderBottomRightRadius':'0 0'
+    const containerStyle = {
+        height: '100%',
+        //display: "flex",
+        //flexDirection: "column",
     }
-    const logoPaperStyle = {
-        padding:20,
-        height:'50vh',
-        'borderTopLeftRadius':'0 0',
-        'borderBottomLeftRadius':'0 0'
+
+    const loginFormStyle = {
+        padding: 20,
+        marginTop: '14vh'
     }
 
     const logoStyle = {
-        'marginTop':'7vh'
+        'marginTop': '8vh',
+        borderRadius: 0,
+        width: '70%',
+        height: "auto",
     }
 
     const lockIconStyle = {
@@ -64,17 +57,17 @@ const LoginPage = () => {
     }
 
     const formStyle = {
-        'marginTop':'8vh'
+        'marginTop': '6vh'
     }
 
     const submitStyle = {
-        'marginTop':'8vh',
-        width:'45%',
+        'marginTop': '8vh',
+        width: '45%',
         'background': 'linear-gradient(129deg, rgba(152,200,100,1) 0%, rgba(5,138,174,1) 84%, rgba(5,90,120,1) 100%)'
     }
 
     const inputStyle = {
-        width:'70%'
+        width: '70%'
     }
 
     const signUpStyle = {
@@ -83,77 +76,60 @@ const LoginPage = () => {
     }
 
     const signUpButtonStyle = {
-        'marginTop':'10px',
-        width:'200px',
+        'marginTop': '10px',
+        width: '200px',
         'background': 'linear-gradient(129deg, rgba(152,200,100,1) 0%, rgba(5,138,174,1) 84%, rgba(5,90,120,1) 100%)',
     }
 
     return (
-        
-    <>
-        
-        <Paper style={backgroundStyle}>
-            <Container>
-                <Grid container marginTop="13vh">
-                    <Grid item xs={6}>
-                        
-                        <Paper style={loginFormStyle}>
-                            <Grid align="center">
-                                <Avatar style={lockIconStyle}><LockOutlinedIcon/></Avatar>
-                                <Typography variant="h5">Connexion</Typography>
-                            </Grid>
-                            <form onSubmit={handleLogin}>
+
+        <Container style={containerStyle}>
+            <Paper>
+                <Grid container justifyContent="space-around" style={loginFormStyle}>
+                    <Grid xs={12} sm={12} md={6} xl={6} align="center">
+                        <Avatar style={lockIconStyle}><LockOutlinedIcon/></Avatar>
+                        <Typography variant="h5">Connexion</Typography>
+                        <form onSubmit={handleLogin}>
                             <Grid align="center" style={formStyle}>
                                 <TextField style={inputStyle}
-                                    required
-                                    id="email"
-                                    label="E-mail"
-                                    variant="standard"
-                                    onChange={handleEmailChange}
+                                           required
+                                           id="email"
+                                           label="E-mail"
+                                           variant="standard"
+                                           onChange={handleEmailChange}
                                 />
-                                <br/>
                                 <TextField style={inputStyle}
-                                    required
-                                    id="password"
-                                    label="Mot de passe"
-                                    type="password"
-                                    autoComplete="current-password"
-                                    variant="standard"
-                                    onChange={handlePasswordChange}
+                                           required
+                                           id="password"
+                                           label="Mot de passe"
+                                           type="password"
+                                           autoComplete="current-password"
+                                           variant="standard"
+                                           onChange={handlePasswordChange}
                                 />
-                                <br/>
                                 <Button type="submit" variant="contained" style={submitStyle}>Se connecter</Button>
                             </Grid>
-                            </form>
-                        </Paper>
-                        
+                        </form>
                     </Grid>
-                    
-                    <Grid item xs={6}>
-                        <Paper>
-                            <Paper style={logoPaperStyle}>
-                                <Grid item style={logoStyle}>
-                                    <Link to="/">
-                                        <img src={logo} width="100%" height="auto"/>
-                                    </Link>
-                                </Grid>
-                                
-                            </Paper>
-                        </Paper>
+
+                    <Grid xs={12} sm={6} md={6} xl={6} align="center">
+                        <Link to="/">
+                            <Avatar src={logo} style={logoStyle}></Avatar>
+                        </Link>
                     </Grid>
+
                 </Grid>
-                <Paper style={signUpStyle}>
-                    <Grid align="center">
-                        <Typography variant="h4">Nouveau ici? Crée ton compte en un clic!</Typography>
-                        <Button href={"/signup"} variant="contained" style={signUpButtonStyle}>S'inscrire</Button>
-                    </Grid>
-                </Paper>
-                
-            </Container>
-                
-        </Paper>
-        
-    </>)
+            </Paper>
+
+            <Paper style={signUpStyle}>
+                <Grid align="center">
+                    <Typography variant="h4">Nouveau ici? Crée ton compte en un clic!</Typography>
+                    <Button href={"/signup"} variant="contained" style={signUpButtonStyle}>S'inscrire</Button>
+                </Grid>
+            </Paper>
+
+        </Container>
+    )
 };
- 
+
 export default LoginPage;
