69f50da23b3768d0cbf8ade300d55eecd55f595f
Thibaut Gérard <thibaut.t.gerard@student.vinci.be>
2021-12-13 18:12:26 +0100

    refactor - change route of navbar button to /login

--- src/src/components/Navbar/Navbar.jsx
+++ src/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import {Avatar, Button, Grid} from "@mui/material";
 import React from "react";
 import {Link} from 'react-router-dom'
 import logo from "../../images/vinci2ndhand.png";
-import logo_vinci from "../../images/icon_logos/logo_vinci2hand.png";
 
 const Navbar = () =>{
     return(
@@ -13,7 +12,7 @@ const Navbar = () =>{
                 </Link>
             </Grid>
             <Grid item>        
-                <Button href={"/signup"} variant="contained" style={{background: 'linear-gradient(129deg, rgba(152,200,100,1) 0%, rgba(5,138,174,1) 84%, rgba(5,90,120,1) 100%)'}}>Se connecter</Button>
+                <Button href={"/login"} variant="contained" style={{background: 'linear-gradient(129deg, rgba(152,200,100,1) 0%, rgba(5,138,174,1) 84%, rgba(5,90,120,1) 100%)'}}>Se connecter</Button>
             </Grid>  
         </Grid>
     )
