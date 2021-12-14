1fd0e8a07a61bf93f2468def75984d3981479bc7
Cezary Mnich <cezary.mnich@student.vinci.be>
2021-12-14 16:07:03 +0100

    fix - merge confilcts

--- src/src/RouterApp.js
+++ src/src/RouterApp.js
@@ -5,11 +5,8 @@ import { Switch, Route, useRouteMatch, Link} from 'react-router-dom'
 import LoginPage from "./Components/LoginPage/LoginPage";
 import ProductsPage from './Components/ProductsPage/ProductsPage';
 import SignUpPage from "./Components/SignUpPage/SignUpPage";
-<<<<<<< HEAD
 import AnnouncementPage from './Components/AnnouncementPage/AnnouncementPage';
-=======
 import CategoriePage from './Components/CategoriePage/CategoriePage';
->>>>>>> 90b2e175b53cd970403ed9e974eb6cd7a19733ad
 
 const RouterApp = () => {
 
@@ -23,16 +20,14 @@ const RouterApp = () => {
                 <Route path={"/signup"}>
                     <SignUpPage />
                 </Route>
-<<<<<<< HEAD
                 <Route path={`/announcement/:id`}>
                     <AnnouncementPage/>
-=======
+                </Route>
                 <Route path={"/products"}>
                     <ProductsPage/>
                 </Route>
                 <Route path={"/categories"}>
                     <CategoriePage/>
->>>>>>> 90b2e175b53cd970403ed9e974eb6cd7a19733ad
                 </Route>
             </Switch>
         </div>
