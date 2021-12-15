395813c3800c383ea4a566a8affe962a83acfc95
Thibaut Gérard <thibaut.t.gerard@student.vinci.be>
2021-12-15 14:53:33 +0100

    refactor - remove unused request in users service

--- src/src/services/users.js
+++ src/src/services/users.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3000/api/users'
 
 
-//Je laisse pour voir comment faire par apres, pour l'instant c'est pas fonctionnel
 const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
@@ -40,33 +39,6 @@ const signUpUser = (user) => {
 const getById = (id) => {
     const request = axios.get(`${baseUrl}/${id}`)
     return request.then(response => response.data)
-}
-
-//Je laisse pour voir comment faire par apres, pour l'instant c'est pas fonctionnel
-const create = (newObject) => {
-    const request = axios.post(baseUrl, newObject)
-    return request.then(response => response.data)
-        .catch(err => {
-            console.log(err)
-        })
-}
-
-//Je laisse pour voir comment faire par apres, pour l'instant c'est pas fonctionnel
-const del = (id) => {
-    const request = axios.delete(`${baseUrl}/${id}`)
-    return request.then(response => response.data)
-        .catch(err => {
-            console.log(err)
-        })
-}
-
-//Je laisse pour voir comment faire par apres, pour l'instant c'est pas fonctionnel
-const update = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
-    return request.then(response => response.data)
-        .catch(err => {
-            console.log(err)
-        })
 }
 
 const addInterest = (categoryId, token) => {
