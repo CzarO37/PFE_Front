a6a4986c1455a350e8972fa88ceb618335f1dbf3
Thibaut Gérard <thibaut.t.gerard@student.vinci.be>
2021-12-15 14:45:07 +0100

    refactor - remove unused request in users service

--- src/src/services/users.js
+++ src/src/services/users.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3000/api/users'
 
 
-//Je laisse pour voir comment faire par apres, pour l'instant c'est pas fonctionnel
 const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
@@ -23,31 +22,4 @@ const getById = (id) => {
     return request.then(response => response.data)
 }
 
-//Je laisse pour voir comment faire par apres, pour l'instant c'est pas fonctionnel
-const create = (newObject) => {
-    const request = axios.post(baseUrl, newObject)
-    return request.then(response => response.data)
-        .catch(err => {
-            console.log(err)
-        })
-}
-
-//Je laisse pour voir comment faire par apres, pour l'instant c'est pas fonctionnel
-const del = (id) => {
-    const request = axios.delete(`${baseUrl}/${id}`)
-    return request.then(response => response.data)
-        .catch(err => {
-            console.log(err)
-        })
-}
-
-//Je laisse pour voir comment faire par apres, pour l'instant c'est pas fonctionnel
-const update = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
-    return request.then(response => response.data)
-        .catch(err => {
-            console.log(err)
-        })
-}
-
-export default {getAll, create, del, update, loginUser, getById, signUpUser}
+export default {getAll, loginUser, getById, signUpUser}
