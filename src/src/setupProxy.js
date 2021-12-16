58652904736ae6730a49ac9438bae513b32373d1
Cezary Mnich <cezary.mnich@student.vinci.be>
2021-12-16 23:43:44 +0100

    fix - proxy url

--- src/src/setupProxy.js
+++ src/src/setupProxy.js
@@ -3,7 +3,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use('/api',
         createProxyMiddleware({
-            target: 'http://localhost:8080',
+            target: 'https://vinci-2nd-hand-prod.herokuapp.com/',
             changeOrigin: true,
         })
         
